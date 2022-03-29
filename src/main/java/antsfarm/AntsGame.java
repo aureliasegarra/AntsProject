51f5b0f8d3240b8652ac9d70bbe2041c2b06772f
Aurelia Segarra <av.segarra@gmail.com>
2022-03-29 15:52:03 +0200

    Ant class ok

--- src/main/java/antsfarm/AntsGame.java
+++ src/main/java/antsfarm/AntsGame.java
@@ -1,7 +1,17 @@
 package antsfarm;
 
+import antsfarm.ants.Ant;
+
 public class AntsGame {
     public static void main(String[] args) {
+        System.out.println("Bonjour");
 
+        Ant f1 = new Ant();
+        Ant f2 = new Ant();
+        Ant f3 = new Ant();
+
+        System.out.println(f1);
+        System.out.println(f2);
+        System.out.println(f3);
     }
 }
--- src/main/java/antsfarm/ants/Ant.java
+++ src/main/java/antsfarm/ants/Ant.java
@@ -1,4 +1,38 @@
 package antsfarm.ants;
 
 public class Ant {
+    private static long counter = 0;
+    private final long number;
+    private final int birthDate;
+
+    public Ant(){
+        super();
+        this.number = ++counter;
+        this.birthDate = 1;
+    }
+
+    public int getAge() {
+        int currentDate = 10;
+        return currentDate-this.birthDate;
+    }
+
+    public boolean isWorker() {
+        return false;
+    }
+
+    public boolean isWarrior() {
+        return false;
+    }
+
+    public boolean isQueen() {
+        return false;
+    }
+
+    @Override
+    public String toString() {
+        return "Ant{" +
+                "number=" + number +
+                ", birthDate=" + birthDate +
+                '}';
+    }
 }
